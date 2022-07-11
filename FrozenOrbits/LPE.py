Fixed sign error in milankovitch LPE

FrozenOrbits/LPE.py
@@ -245,9 +245,9 @@ class LPE_Milankovitch():
             dUde = dUdOE[:,3:6]
             dUdl = dUdOE[:,6]
             
-            p = element_dot(H, H)/mu # TODO: non-dimensionalize mu
+            p = element_dot(H, H)/mu 
             e_squared = element_dot(e,e)
-            denominator = tf.cond(e_squared >= 1.0, lambda: (e_squared - 1), lambda: (1 - e_squared))
+            denominator = tf.cond(e_squared >= 1.0, lambda: (e_squared - 1.0), lambda: (1.0 - e_squared))
             a = p/denominator
             n = self._compute_n(a)
 
@@ -276,10 +276,10 @@ class LPE_Milankovitch():
                     term1*dUdl)
             e_dt = (1.0/self.m_star)*(
                     tf.linalg.cross(eVec, dUdH) + 
-                    term2*tf.linalg.cross(HVec,dUde) + 
+                    term2*tf.linalg.cross(HVec,dUde) - 
                     term6*dUdl)
             L_dt = (1.0/self.m_star)*(
-                    -element_dot(term1,dUdH) - 
+                    -element_dot(term1,dUdH) + 
                     element_dot(term6, dUde)) + \
                     n 
 
@@ -337,10 +337,10 @@ class LPE_Milankovitch():
                 term1*dUdl)
         e_dt = (1.0/self.m_star)*(
                 tf.linalg.cross(eVec, dUdH) + 
-                term2*tf.linalg.cross(HVec,dUde) + 
+                term2*tf.linalg.cross(HVec,dUde) - 
                 term6*dUdl)
         L_dt = (1.0/self.m_star)*(
-                -element_dot(term1,dUdH) - 
+                -element_dot(term1,dUdH) + 
                 element_dot(term6, dUde)) + \
                 n 
 
@@ -472,7 +472,7 @@ class LPE_Traditional():
                 trad_OE_dim = self.dimensionalize_state(OE)
                 cart_state = oe2cart_tf(trad_OE_dim, self.mu_tilde)
                 r_tilde = cart_state[:,0:3]
-                u_pred_tilde = self.generate_potential(r_tilde)
+                u_pred_tilde = -self.generate_potential(r_tilde)
                 u_pred = (self.t_star/self.l_star)**2*self.m_star*u_pred_tilde
             dUdOE = tape_dt.gradient(u_pred, OE)
                 
@@ -503,7 +503,6 @@ class LPE_Traditional():
 
         dOE_dt_dx = tape.batch_jacobian(dOE_dt, OE, experimental_use_pfor=False)
         return dOE_dt_dx
-
 
 class LPE_Cartesian():
     "LPE with cartesian coordinates, but normalized by length, time, and mass"
@@ -597,3 +596,135 @@ class LPE_Cartesian():
 
         dOE_dt_dx = tape.batch_jacobian(dOE_dt, OE, experimental_use_pfor=False)
         return dOE_dt_dx
+
+class LPE_Equinoctial():
+    def __init__(self, model, mu, l_star=1.0, t_star=1.0, m_star=1.0):
+        self.model = model
+        self.G_tilde = 6.67408*10E-11 # m^3/(kg*s^2)
+        self.mu_tilde = tf.constant(mu, dtype=tf.float64, name='mu')
+        self.l_star = tf.constant(l_star, dtype=tf.float64, name='ref_length')
+        self.m_star = tf.constant(m_star, dtype=tf.float64, name='ref_mass')
+        self.t_star = tf.constant(t_star, dtype=tf.float64, name='ref_time')
+        
+        self.mu = self.mu_tilde * (self.t_star**2/ self.l_star**3)
+        self.element_set = 'equinoctial'
+        self.num_elements = 6
+
+    def non_dimensionalize_state(self, x0):
+        p_non_dim = tf.reshape(x0[:,0]/self.l_star, (-1, 1))
+        equi_OE_ND = tf.concat([p_non_dim, x0[:,1:]], axis=1)
+        return equi_OE_ND
+
+    def dimensionalize_state(self, x0):
+        p_dim = tf.reshape(x0[:,0]*self.l_star, (-1, 1))
+        equi_OE = tf.concat([p_dim, x0[:,1:]], axis=1)
+        return equi_OE
+
+    def non_dimensionalize_time(self, T):
+        T_ND = T/self.t_star
+        return T_ND
+
+    def dimensionalize_time(self, T):
+        T = T*self.t_star
+        return T
+
+    def _conditional_compute_n(self, a):
+        n = tf.cond(tf.greater_equal(a,0), lambda: tf.sqrt(self.mu/a**3), lambda: tf.sqrt(-self.mu/a**3))
+        return n
+
+    def _compute_n(self, a):
+        n = tf.map_fn(fn= lambda a : self._conditional_compute_n(a), elems=(a))
+        return n 
+
+    def dOE_dt(self,equi_OE): 
+        equi_OE_input = equi_OE.reshape((1, -1)).astype(np.float64)
+        equi_OE_input_tf = tf.Variable(equi_OE_input, dtype=tf.float64, name='orbit_elements')
+        dOE_dt_results = self.dOE_equinoctial_dt(equi_OE_input_tf, self.mu)
+        return dOE_dt_results.numpy()
+
+    def dOE_dt_dx(self,equi_OE): 
+        equi_OE_input = equi_OE.reshape((1, -1)).astype(np.float64)
+        equi_OE_input_tf = tf.Variable(equi_OE_input, dtype=tf.float64, name='orbit_elements')
+        dOE_dt_dx_results = self.dOE_equinoctial_dt_dx(equi_OE_input_tf, self.mu)
+        return dOE_dt_dx_results.numpy()
+
+    def generate_potential(self,x):
+        """
+        Very silly function, but it's the only way to compile dOE_equinoctial_dt_dx as a tf.function().
+        In short, tf.function is unable to accept the model as an argument without constant retracing
+        according to: https://github.com/tensorflow/tensorflow/issues/38875
+        """
+        U_tilde = self.model.generate_potential_tf(x)
+        return U_tilde
+
+    @tf.function(input_signature=[tf.TensorSpec(shape=(None, 6), dtype=tf.float64), tf.TensorSpec(shape=(), dtype=tf.float64)])
+    def dOE_equinoctial_dt_dx(self, equinoctial_OE, mu): 
+        print("Retracing")
+
+        with tf.GradientTape(persistent=True) as tape_dx:
+            tape_dx.watch(equinoctial_OE)
+            with tf.GradientTape(persistent=True) as tape:
+                tape.watch(equinoctial_OE) 
+                equi_OE_dim = self.dimensionalize_state(equinoctial_OE)
+                trad_OE = equinoctial2oe_tf(equi_OE_dim, self.mu_tilde)
+                cart_state = oe2cart_tf(trad_OE, self.mu_tilde)     
+                r_tilde = cart_state[:,0:3]
+                u_pred_tilde = -self.generate_potential(r_tilde)
+                U = (self.t_star/self.l_star)**2*self.m_star*u_pred_tilde
+            dUdOE = tape.gradient(U, equinoctial_OE)
+
+            p = equinoctial_OE[:,0]
+            f = equinoctial_OE[:,1]
+            g = equinoctial_OE[:,2]
+            L = equinoctial_OE[:,3]
+            h = equinoctial_OE[:,4]
+            k = equinoctial_OE[:,5]
+
+            s = tf.sqrt(1. + h**2 + k**2)
+            w = 1. + f*tf.cos(L) + g*tf.sin(L)
+            # p, f, g, L, h, k
+            
+            dpdt = 2.0*tf.sqrt(p/mu) * (-g*dUdOE[:,1] + f*dUdOE[:,2] + dUdOE[:,3]),
+            dfdt = 1.0/tf.sqrt(mu*p)*(2.*p*g*dUdOE[:,0] - (1. - f**2 - g**2)*dUdOE[:,2] - g*s**2/2.0*(h*dUdOE[:,4] + k*dUdOE[:,5]) + (f+(1.0+w)*tf.cos(L))*dUdOE[:,3]),
+            dgdt = 1.0/tf.sqrt(mu*p)*(-2.*p*f*dUdOE[:,0] + (1. - f**2 - g**2)*dUdOE[:,1] + f*s**2/2.0*(h*dUdOE[:,4] + k*dUdOE[:,5]) + (g+(1.0+w)*tf.sin(L))*dUdOE[:,3]),
+            dLdt = tf.sqrt(mu*p)*(w/p)**2 + s**2/(2.0*tf.sqrt(mu*p))*(h*dUdOE[:,4] + k*dUdOE[:,5]),
+            dhdt = s**2/(2.0*tf.sqrt(mu*p))*(h*(g*dUdOE[:,1] - f*dUdOE[:,2] - dUdOE[:,3]) - s**2/2.0*dUdOE[:,5]),
+            dkdt = s**2/(2.0*tf.sqrt(mu*p))*(k*(g*dUdOE[:,1] - f*dUdOE[:,2] - dUdOE[:,3]) + s**2/2.0*dUdOE[:,4])
+            dOE_dt = tf.concat([dpdt, dfdt, dgdt,dLdt, dhdt, tf.reshape(dkdt, (-1,1))],axis=1)    
+        dOE_dt_dx = tape_dx.batch_jacobian(dOE_dt, equinoctial_OE, experimental_use_pfor=False)
+        return dOE_dt_dx[0]
+
+    @tf.function(input_signature=[tf.TensorSpec(shape=(None, 6), dtype=tf.float64),tf.TensorSpec(shape=None, dtype=tf.float64)])
+    def dOE_equinoctial_dt(self, equinoctial_OE, mu): 
+
+        with tf.GradientTape(persistent=True) as tape:
+            tape.watch(equinoctial_OE) 
+            equi_OE_dim = self.dimensionalize_state(equinoctial_OE)
+            trad_OE = equinoctial2oe_tf(equi_OE_dim, self.mu_tilde)
+            cart_state = oe2cart_tf(trad_OE, self.mu_tilde)
+            r_tilde = cart_state[:,0:3]
+            u_pred_tilde = -self.generate_potential(r_tilde)
+            U = (self.t_star/self.l_star)**2*self.m_star*u_pred_tilde
+        dUdOE = tape.gradient(U, equinoctial_OE)
+
+        p = equinoctial_OE[:,0]
+        f = equinoctial_OE[:,1]
+        g = equinoctial_OE[:,2]
+        L = equinoctial_OE[:,3]
+        h = equinoctial_OE[:,4]
+        k = equinoctial_OE[:,5]
+
+        s = tf.sqrt(1. + h**2 + k**2)
+        w = 1. + f*tf.cos(L) + g*tf.sin(L)
+        # p, f, g, L, h, k
+        
+        dpdt = 2.0*tf.sqrt(p/mu) * (-g*dUdOE[:,1] + f*dUdOE[:,2] + dUdOE[:,3]),
+        dfdt = 1.0/tf.sqrt(mu*p)*(2.*p*g*dUdOE[:,0] - (1. - f**2 - g**2)*dUdOE[:,2] - g*s**2/2.0*(h*dUdOE[:,4] + k*dUdOE[:,5]) + (f+(1.0+w)*tf.cos(L))*dUdOE[:,3]),
+        dgdt = 1.0/tf.sqrt(mu*p)*(-2.*p*f*dUdOE[:,0] + (1. - f**2 - g**2)*dUdOE[:,1] + f*s**2/2.0*(h*dUdOE[:,4] + k*dUdOE[:,5]) + (g+(1.0+w)*tf.sin(L))*dUdOE[:,3]),
+        dLdt = tf.sqrt(mu*p)*(w/p)**2 + s**2/(2.0*tf.sqrt(mu*p))*(h*dUdOE[:,4] + k*dUdOE[:,5]),
+        dhdt = s**2/(2.0*tf.sqrt(mu*p))*(h*(g*dUdOE[:,1] - f*dUdOE[:,2] - dUdOE[:,3]) - s**2/2.0*dUdOE[:,5]),
+        dkdt = s**2/(2.0*tf.sqrt(mu*p))*(k*(g*dUdOE[:,1] - f*dUdOE[:,2] - dUdOE[:,3]) + s**2/2.0*dUdOE[:,4])
+
+        dOE_dt = tf.concat([dpdt, dfdt, dgdt,dLdt, dhdt, tf.reshape(dkdt, (-1,1))],axis=1)      
+
+        return dOE_dt[0]

Scripts/BVP/scipy_bvp_mil_OE.py
@@ -1,5 +1,5 @@
 import os
-from FrozenOrbits.analysis import check_for_intersection, print_state_differences
+from FrozenOrbits.analysis import check_for_intersection, print_OE_differences, print_state_differences
 from FrozenOrbits.bvp import *
 
 import GravNN
@@ -14,13 +14,7 @@ from GravNN.CelestialBodies.Asteroids import Eros
 from FrozenOrbits.constraints import *
 from initial_conditions import * 
 
-def get_initial_conditions(mu, element_set):
-    # OE_trad = np.array([[7.93E+04/2, 1.00E-02, 1.53E+00, -7.21E-01, -1.61E-01, 2.09e+00]])
-    OE_trad = np.array([[7.93E+04/2, 1.00E-01, 1.53E+00/2, -7.21E-01, -1.61E-01, 2.09e+00]])
-    T = 2*np.pi*np.sqrt(OE_trad[0,0]**3/mu)
-    OE_0 = oe2milankovitch_tf(OE_trad, mu).numpy()
-    X_0 = oe2cart_tf(OE_0, mu, element_set)[0]
-    return OE_0, X_0, T
+
 
 def main():
     """Solve a BVP problem using the dynamics of the cartesian state vector"""
@@ -28,9 +22,15 @@ def main():
     np.random.seed(15)
     # tf.config.run_functions_eagerly(True)
 
-    element_set = 'milankovitch'
     OE_0, X_0, T, planet = near_periodic_IC()
     OE_0 = oe2milankovitch_tf(OE_0, planet.mu).numpy()
+
+    # If manual coordinate needed, rewrap OE
+    OE_0 = np.array([[-1.49e+04, -9.01e+04, 9.56e+04, 5.95e-02, -7.18e-02, -4.56e-02, 1.21e+00]])
+    T = 73697.6367377903
+    X_0 = oe2cart_tf(OE_0, planet.mu, 'milankovitch').numpy()
+    OE_0 = cart2oe_tf(X_0, planet.mu, 'milankovitch').numpy()
+    X_0 = X_0[0]
 
     model = pinnGravityModel(os.path.dirname(GravNN.__file__) + \
         "/../Data/Dataframes/eros_BVP_PINN_III.data")  
@@ -38,29 +38,34 @@ def main():
     # Desire |H_0| = 1
     # Acknowledging H = (t_star/l_star**2)*H_tilde 
     # Solve for l_star -> l_star = sqrt(t_star*H_tilde_mag/H_mag) 
+
+    H_mag_desired = 1.0
     lpe = LPE_Milankovitch(model.gravity_model, planet.mu, 
-                            l_star=np.sqrt(T*np.linalg.norm(OE_0[0,0:3])/1.0),  # H = t/l**2 * H_tilde
+                            l_star=np.sqrt(T*np.linalg.norm(OE_0[0,0:3])/H_mag_desired),  # H = t/l**2 * H_tilde
                             t_star=T, 
                             m_star=1.0)
 
 
     # normalized coordinates for semi-major axis
-    bounds = ([-2*1/np.sqrt(3), -2*1/np.sqrt(3), -2*1/np.sqrt(3), -1.1, -1.1, -1.1, -np.inf],
-              [ 2*1/np.sqrt(3),  2*1/np.sqrt(3),  2*1/np.sqrt(3),  1.1,  1.1,  1.1,  np.inf])
+    # bounds = ([-2*1/np.sqrt(3), -2*1/np.sqrt(3), -2*1/np.sqrt(3), -0.3, -0.3, -0.3, -np.inf],
+    #           [ 2*1/np.sqrt(3),  2*1/np.sqrt(3),  2*1/np.sqrt(3),  0.3,  0.3,  0.3,  np.inf])
               
-    bounds = ([ -np.inf,  -np.inf,  -np.inf,  -np.inf,  -np.inf,  -np.inf, -np.inf, -np.inf],
-              [  np.inf,   np.inf,   np.inf,   np.inf,   np.inf,   np.inf,  np.inf, np.inf])
+    bounds = ([ -1.0*H_mag_desired,  -1.0*H_mag_desired,  -1.0*H_mag_desired, -0.7,  -0.7, -0.7, -np.inf, 0.9],
+              [  1.0*H_mag_desired,   1.0*H_mag_desired,   1.0*H_mag_desired, 0.7,   0.7,  0.7, np.inf, 1.1])
 
 
+    # Shooting solvers
+    start_time = time.time()
     decision_variable_mask = [True, True, True, True, True, True, True, True] # [OE, T] [N+1]
-    constraint_angle_wrap_mask = [False, False, False, False, False, False, True] # wrap w, Omega, M # 
+    constraint_variable_mask = [True, True, True, True, True, True, True, False] #Don't use period as part of the constraint, just as part of jacobian
+    constraint_angle_wrap_mask = [False, False, False, False, False, False, True, False]
+    solver = ShootingLsSolver(lpe, decision_variable_mask, constraint_variable_mask, constraint_angle_wrap_mask) # Finds a local optimum, step size gets too small
 
-    OE_0_sol, X_0_sol, T_sol = scipy_periodic_orbit_algorithm_v2(T, OE_0, lpe, 
-                                            bounds, element_set, decision_variable_mask, 
-                                            constraint_angle_wrap_mask=constraint_angle_wrap_mask)
-
-    print(f"Initial OE: {OE_0}")
-    print(f"BVP OE: {OE_0_sol}")
+    OE_0_sol, X_0_sol, T_sol, results = solver.solve(OE_0, T, bounds)
+    
+    print(f"Time Elapsed: {time.time()-start_time}")
+    print(f"Initial OE: {OE_0} \t T: {T}")
+    print(f"BVP OE: {OE_0_sol} \t T {T_sol}")
 
     # propagate the initial and solution orbits
     init_sol = propagate_orbit(T, X_0, model, tol=1E-7) 
@@ -77,6 +82,14 @@ def main():
     plot_cartesian_state_3d(bvp_sol.y.T, planet.obj_8k)
     plt.title("BVP Solution")
 
+    OE_trad_init = cart2oe_tf(init_sol.y.T, planet.mu, lpe.element_set).numpy()
+    OE_trad_bvp = cart2oe_tf(bvp_sol.y.T, planet.mu, lpe.element_set).numpy()
+
+    print_OE_differences(OE_trad_init, lpe, "IVP", constraint_angle_wrap_mask)
+    print_OE_differences(OE_trad_bvp, lpe, "BVP", constraint_angle_wrap_mask)
+
+    plot_OE_1d(init_sol.t, OE_trad_init, lpe.element_set, y0_hline=True)
+    plot_OE_1d(bvp_sol.t, OE_trad_bvp, lpe.element_set, y0_hline=True)
     plt.show()
 
 

Scripts/Tests/test_milankovitch_conservation_of_energy.py
@@ -11,8 +11,27 @@ from FrozenOrbits.utils import propagate_orbit
 from FrozenOrbits.visualization import *
 from GravNN.CelestialBodies.Asteroids import Eros
 
+def plot_milankovitch_OE(OE):
+    """OE [N x 6]"""
+    plt.figure()
+    plt.subplot(4,2,1)
+    plt.plot(OE[:,0])
+    plt.subplot(4,2,2)
+    plt.plot(OE[:,1])
+    plt.subplot(4,2,3)
+    plt.plot(OE[:,2])
+    plt.subplot(4,2,4)
+    plt.plot(OE[:,3])
+    plt.subplot(4,2,5)
+    plt.plot(OE[:,4])
+    plt.subplot(4,2,6)
+    plt.plot(OE[:,5])
+    plt.subplot(4,2,7)
+    plt.plot(OE[:,6])
+
 def sample_mirror_orbit(R, mu):
-    OE_trad = np.array([[7.93E+04/2, 1.00E-02, 1.53E+00, -7.21E-01, -1.61E-01, 2.09e+00]])
+    # OE_trad = np.array([[7.93E+04/2, 1.00E-02, 1.53E+00, -7.21E-01, -1.61E-01, 2.09e+00]]) # Problems when eVec -> 0.0, there is some numerical challenges
+    OE_trad = np.array([[7.93E+04/2, 1.00E-01, 1.53E+00, -7.21E-01, -1.61E-01, 2.09e+00]])
     T = 2*np.pi*np.sqrt(OE_trad[0,0]**3/mu)
     OE = oe2milankovitch_tf(OE_trad, mu).numpy()
     return OE, T
@@ -54,15 +73,31 @@ def main():
 
     OE_sol.y = lpe.dimensionalize_state(OE_sol.y.T).numpy().T
     OE_sol.t = lpe.dimensionalize_time(OE_sol.t).numpy()
+    plot_milankovitch_OE(OE_sol.y.T)
+    plt.suptitle("OE from Integrated OE0")
+
+    # Convert OE solution to cartesian
+    OE_2_cart_sol = copy.deepcopy(OE_sol)
+    OE_2_cart_sol.y = oe2cart_tf(OE_sol.y.T, planet.mu, element_set).numpy().T
 
     # Propagate cartesian state using cartesian accelerations
-    R_0, V_0 = oe2cart_tf(OE_0_dim, planet.mu, element_set)
-    x_0 = np.hstack((R_0.numpy(), V_0.numpy()))[0]
+    x_0 = oe2cart_tf(OE_0_dim, planet.mu, element_set)[0]
     cart_sol = propagate_orbit(T_dim, x_0, model, tol=1E-7) 
+    cart_sol_OE = cart2milankovitch_tf(cart_sol.y.T, planet.mu)
 
+    plot_milankovitch_OE(cart_sol_OE)
+    plt.suptitle("OE from Integrated X0")
+
+    # Plot differences between the solutions
+    dOE = OE_sol.y.T - cart_sol_OE
+    plot_milankovitch_OE(dOE)
+    plt.suptitle("delta OE")
 
     plot_energy(cart_sol, model)
-    plot_energy(OE_sol, model)
+    plot_energy(OE_2_cart_sol, model)
+
+    plot_3d_trajectory(cart_sol, Eros().obj_8k)
+    plot_3d_trajectory(OE_2_cart_sol, Eros().obj_8k)
 
     plt.show()
 