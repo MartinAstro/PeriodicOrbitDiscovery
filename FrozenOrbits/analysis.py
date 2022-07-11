3213910a54ec733faca7f2ec1541961ca1502000
John M <john.r.martin@colorado.edu>
2022-07-11 17:48:10 -0600

    Return element differences now

--- FrozenOrbits/analysis.py
+++ FrozenOrbits/analysis.py
@@ -1,6 +1,8 @@
 import numpy as np
 import trimesh
 
+from FrozenOrbits.utils import calc_angle_diff
+
 def print_state_differences(sol):
     pos_diff = np.linalg.norm(sol.y[0:3, -1] - sol.y[0:3, 0])
     vel_diff = np.linalg.norm(sol.y[3:6, -1] - sol.y[3:6, 0])
@@ -9,7 +11,7 @@ def print_state_differences(sol):
     print(f"Integrated Velocity Difference {vel_diff} [m/s]")
 
 
-def print_OE_differences(OE_sol, lpe, prefix):
+def print_OE_differences(OE_sol, lpe, prefix, constraint_angle_wrap):
 
     OE_dim_start = OE_sol[0,:]
     OE_dim_end = OE_sol[-1,:]
@@ -18,9 +20,16 @@ def print_OE_differences(OE_sol, lpe, prefix):
     OE_start = lpe.non_dimensionalize_state(OE_dim_start.reshape((1,-1))).numpy()[0]
     OE_end = lpe.non_dimensionalize_state(OE_dim_end.reshape((1,-1))).numpy()[0]
     OE_diff = OE_end - OE_start
+
+    for i in range(len(OE_end)):
+        if constraint_angle_wrap[i]:
+            OE_diff[i] = calc_angle_diff(OE_start[i], OE_end[i])
+            OE_dim_diff[i] = calc_angle_diff(OE_dim_start[i], OE_dim_end[i])
     
     print(f"{prefix} OE Differences {OE_dim_diff} \t Total dOE: {np.linalg.norm(OE_dim_diff)}")
     print(f"{prefix} OE Non-Dim Differences {OE_diff} \t Total dOE: {np.linalg.norm(OE_diff)}")
+
+    return OE_dim_diff, OE_diff
 
 
 def check_for_intersection(sol, obj_file):
