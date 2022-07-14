e0cbc05c0ed8003a40577e1f6a3f98963dd7c210
John M <john.r.martin@colorado.edu>
2022-07-14 16:04:38 -0600

    Improve figures for paper

--- Scripts/Figures/figure_constrained_initial_conditions.py
+++ Scripts/Figures/figure_constrained_initial_conditions.py
@@ -1,3 +1,4 @@
+from locale import D_FMT
 import os
 import copy
 import time
@@ -22,37 +23,88 @@ from Scripts.BVP.initial_conditions import *
 
 def sample_initial_conditions(df, k):
     planet = Eros()
-    OE_0 = np.array([df["OE_0_sol"][k]])
-    T_0 = df["T_0_sol"][k]
-    X_0 = np.array(df["X_0_sol"][k])
+    T_0= df.iloc[k]["T_0"]
+    X_0 = np.array(df.iloc[k]["X_0"])
+    T_0_sol = df.iloc[k]["T_0_sol"]
+    X_0_sol = np.array(df.iloc[k]["X_0_sol"])
 
     # Don't include the bounded OE scenario
     # replace with the initial conditions instead
     if df.index[k] == "OE_Bounded":
-        OE_0 = np.array([df["OE_0"][k]])
-        T_0 = df["T_0"][k]
-        X_0 = np.array(df["X_0"][k])
+        T_0= df.iloc[k]["T_0"]
+        X_0 = np.array(df.iloc[k]["X_0"])
+        T_0_sol = df.iloc[k]["T_0"]
+        X_0_sol = np.array(df.iloc[k]["X_0"])
 
-    return OE_0, X_0, T_0, planet
+    return T_0, X_0, T_0_sol, X_0_sol, planet
+
+def plot_integrated_orbits(df, base_figure_name):
+    vis = VisualizationBase()
+    model = pinnGravityModel(os.path.dirname(GravNN.__file__) + \
+        "/../Data/Dataframes/eros_BVP_PINN_III.data")  
+    solid_color_list = ['blue', 'red', 'green']
+    color_list = [plt.cm.Blues, plt.cm.Reds, plt.cm.Greens]
+    
+    OE_fig, OE_ax = vis.newFig() # for future use
+
+    color_idx = 0
+    for k in range(len(df)):
+        T_0, X_0, T_0_sol, X_0_sol, planet = sample_initial_conditions(df, k)
+
+
+        # The original IC should be in black
+        experiment_name = df.index[k]
+        if experiment_name == 'OE_bounded':
+            color = 'black'
+        else:
+            color = solid_color_list[color_idx]
+            color_idx += 1
+
+        if experiment_name != "OE_bounded":
+            # Original Orbit
+            t_eval_original = np.linspace(0, T_0, 100)
+            original_orbit_sol = propagate_orbit(
+                T_0, 
+                X_0, 
+                model, 
+                tol=1E-7,
+                t_eval=t_eval_original) 
+            plot_cartesian_state_3d(original_orbit_sol.y.T, planet.obj_8k, new_fig=True, solid_color='black', line_opacity=0.5)
+            
+            # Corrected Orbit
+            t_eval_solution = np.linspace(0, T_0_sol, 100)
+            solution_orbit_sol = propagate_orbit(
+                T_0_sol, 
+                X_0_sol, 
+                model, 
+                tol=1E-7,
+                t_eval=t_eval_solution) 
+            plot_cartesian_state_3d(solution_orbit_sol.y.T, None, new_fig=False, solid_color=color)
+            vis.save(plt.gcf(), f"{base_figure_name}_{color_idx}_corrected.pdf")
+        else:
+            t_eval_solution = np.linspace(0, T_0_sol, 100)
+            solution_orbit_sol = propagate_orbit(
+                T_0_sol, 
+                X_0_sol, 
+                model, 
+                tol=1E-7,
+                t_eval=t_eval_solution) 
+        plt.figure(OE_fig.number)
+        OE = cart2oe_tf(solution_orbit_sol.y.T, planet.mu).numpy()
+        plot_OE_1d(solution_orbit_sol.t, OE, "traditional", 
+                    color=color, 
+                    new_fig=False, 
+                    label=experiment_name, 
+                    horizontal=True)
+    vis.save(plt.gcf(), f"{base_figure_name}_OE.pdf")
+
 
 
 def main():
-    model = pinnGravityModel(os.path.dirname(GravNN.__file__) + \
-        "/../Data/Dataframes/eros_BVP_PINN_III.data")  
-
     directory =  os.path.dirname(FrozenOrbits.__file__)+ "/Data/"
     df = pd.read_pickle(directory + "constrained_orbit_solutions.data")
-    vis = VisualizationBase()
-    period_multiplier = 1
-    for k in range(len(df)):
-        new_fig = True if k == 0 else False
-        OE_0, X_0, T, planet = sample_initial_conditions(df, k)           
-        init_sol = propagate_orbit(T*period_multiplier, X_0, model, tol=1E-7, t_eval=np.linspace(0, T*period_multiplier, 100*period_multiplier)) 
-        plot_cartesian_state_3d(init_sol.y.T, planet.obj_8k, new_fig=new_fig)
-        plt.title("Initial Conditions")
-
-
-    vis.save(plt.gcf(), os.path.basename(__file__).split('.py')[0] + '.pdf')
+    base_figure_name = os.path.basename(__file__).split('.py')[0]
+    plot_integrated_orbits(df, base_figure_name)
     plt.show()
 
 if __name__ == "__main__":
--- Scripts/Figures/figure_integrated_IC.py
+++ Scripts/Figures/figure_integrated_IC.py
@@ -22,29 +22,71 @@ from Scripts.BVP.initial_conditions import *
 
 def sample_initial_conditions(df, k):
     planet = Eros()
-    OE_0 = np.array([df["OE_0_sol"][k]])
-    T_0 = df["T_0_sol"][k]
-    X_0 = np.array(df["X_0_sol"][k])
-    return OE_0, X_0, T_0, planet
+    T_0= df.iloc[k]["T_0"]
+    X_0 = np.array(df.iloc[k]["X_0"])
+    T_0_sol = df.iloc[k]["T_0_sol"]
+    X_0_sol = np.array(df.iloc[k]["X_0_sol"])
+    return T_0, X_0, T_0_sol, X_0_sol, planet
+
+
+def plot_integrated_orbits(coarse_df, fine_df, base_figure_name):
+    vis = VisualizationBase()
+    model = pinnGravityModel(os.path.dirname(GravNN.__file__) + \
+        "/../Data/Dataframes/eros_BVP_PINN_III.data")  
+    solid_color_list = ['blue', 'red', 'green']
+    color_list = [plt.cm.Blues, plt.cm.Reds, plt.cm.Greens]
+    for k in range(3):
+        T_0, X_0, _, _, planet = sample_initial_conditions(coarse_df, k)
+        _, _, T_0_sol, X_0_sol, planet = sample_initial_conditions(fine_df, k)
+
+        # Original Orbit
+        t_eval_original = np.linspace(0, T_0, 100)
+        original_orbit_sol = propagate_orbit(
+            T_0, 
+            X_0, 
+            model, 
+            tol=1E-7,
+            t_eval=t_eval_original) 
+        plot_cartesian_state_3d(original_orbit_sol.y.T, planet.obj_8k, new_fig=True, solid_color='black', line_opacity=0.5)
+        
+        # Corrected Orbit
+        t_eval_solution = np.linspace(0, T_0_sol, 100)
+        solution_orbit_sol = propagate_orbit(
+            T_0_sol, 
+            X_0_sol, 
+            model, 
+            tol=1E-7,
+            t_eval=t_eval_solution) 
+        plot_cartesian_state_3d(solution_orbit_sol.y.T, None, new_fig=False, solid_color=solid_color_list[k])
+        vis.save(plt.gcf(), f"{base_figure_name}_{k}_corrected.pdf")
+
+        # Corrected Orbit Propagated for 10 periods
+        period_multiplier = 10
+        t_eval_solution = np.linspace(0, T_0_sol*period_multiplier, 100*period_multiplier)
+        solution_orbit_sol = propagate_orbit(
+            T_0_sol*period_multiplier, 
+            X_0_sol, 
+            model, 
+            tol=1E-7,
+            t_eval=t_eval_solution) 
+        plot_cartesian_state_3d(solution_orbit_sol.y.T, planet.obj_8k, new_fig=True, cmap=color_list[k])
+        vis.save(plt.gcf(), f"{base_figure_name}_{k}_corrected_x10.pdf")
 
 
 def main():
-    model = pinnGravityModel(os.path.dirname(GravNN.__file__) + \
-        "/../Data/Dataframes/eros_BVP_PINN_III.data")  
 
     directory =  os.path.dirname(FrozenOrbits.__file__)+ "/Data/"
     coarse_df = pd.read_pickle(directory + "coarse_orbit_solutions.data")
-    # coarse_df = pd.read_pickle(directory + "fine_orbit_solutions.data")
+    fine_df = pd.read_pickle(directory + "fine_orbit_solutions.data")
+    base_figure_name = os.path.basename(__file__).split('.py')[0]
+    plot_integrated_orbits(coarse_df, fine_df, base_figure_name)
 
-    period_multiplier = 10
-    for k in range(len(coarse_df)):
-        new_fig = True if k == 0 else False
-        OE_0, X_0, T, planet = sample_initial_conditions(coarse_df, k)
-        init_sol = propagate_orbit(T*period_multiplier, X_0, model, tol=1E-7, t_eval=np.linspace(0, T*period_multiplier, 100*period_multiplier)) 
-        plot_cartesian_state_3d(init_sol.y.T, planet.obj_8k, new_fig=new_fig)
-        plt.title("Initial Conditions")
+    coarse_df = pd.read_pickle(directory + "cartesian_coarse_orbit_solutions.data")
+    fine_df = pd.read_pickle(directory + "cartesian_fine_orbit_solutions.data")
+    base_figure_name = os.path.basename(__file__).split('.py')[0] + "_cart"
+    plot_integrated_orbits(coarse_df, fine_df, base_figure_name)
 
-    plt.show()
+    # plt.show()
 
 if __name__ == "__main__":
     main()
