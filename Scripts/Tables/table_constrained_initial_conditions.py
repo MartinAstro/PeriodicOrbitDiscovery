25c27eb4271ae0fbcfe52f8876ef4fe7df888de4
John M <john.r.martin@colorado.edu>
2022-07-20 16:38:22 -0600

    Fixed table for constrained IC

--- Scripts/Tables/table_constrained_initial_conditions.py
+++ Scripts/Tables/table_constrained_initial_conditions.py
@@ -20,31 +20,32 @@ from FrozenOrbits.constraints import *
 from Scripts.BVP.initial_conditions import *
 np.set_printoptions(formatter={'float': "{0:0.2e}".format})
 
-def sample_initial_conditions(df, k):
-    planet = Eros()
-    OE_0 = np.array([df.iloc[k]["OE_0_sol"]])
+def get_original_IC(df, k):
+    T_0 = df.iloc[k]["T_0"]
+    X_0 = np.array(df.iloc[k]["X_0"])
+    return T_0, X_0
+
+def get_corrected_IC(df, k):
     T_0 = df.iloc[k]["T_0_sol"]
     X_0 = np.array(df.iloc[k]["X_0_sol"])
-
-    # Don't include the bounded OE scenario
-    # replace with the initial conditions instead
-    if df.index[k] == "OE_Bounded":
-        OE_0 = np.array([df.iloc[k]["OE_0"]])
-        T_0 = df.iloc[k]["T_0"]
-        X_0 = np.array(df.iloc[k]["X_0"])
-
-    return OE_0, X_0, T_0, planet
-
+    return T_0, X_0
 
 def main():
     directory =  os.path.dirname(FrozenOrbits.__file__)+ "/Data/"
     df = pd.read_pickle(directory + "constrained_orbit_solutions.data")
-    for k in range(len(df)):
-        new_fig = True if k == 0 else False
-        OE_0, X_0, T, planet = sample_initial_conditions(df, k)           
+    planet = Eros()
+    # Print the original initial condition first
+    T_0, X_0 = get_original_IC(df, 0) # use 0 b/c they all share same IC
+    OE_0 = cart2oe_tf(np.array([X_0]), planet.mu)
+    print(f"Initial Condition & {OE_0[0,0]} & {OE_0[0,1]} & {OE_0[0,2]} & {OE_0[0,3]} & {OE_0[0,4]} & {OE_0[0,5]} & {T_0} \\\\ \n \\hline ")
 
+
+    for k in range(len(df)):
+        if df.index[k] == "OE_bounded":
+            continue
+        T_0_sol, X_0_sol = get_corrected_IC(df, k)
+        OE_0 = cart2oe_tf(np.array([X_0_sol]), planet.mu)
         if df.index[k] == 'cartesian':
-            OE_0 = cart2oe_tf(OE_0, planet.mu)
             label = "Cartesian"
         elif df.index[k] == "OE_bounded":
             label = "Initial Condition"
@@ -53,7 +54,7 @@ def main():
         elif df.index[k] == "OE":
             label = "Orbital Elements"
 
-        print(f"{label} & {OE_0[0,0]} & {OE_0[0,1]} & {OE_0[0,2]} & {OE_0[0,3]} & {OE_0[0,4]} & {OE_0[0,5]} & {T} \\\\ \n \\hline ")
+        print(f"{label} & {OE_0[0,0]} & {OE_0[0,1]} & {OE_0[0,2]} & {OE_0[0,3]} & {OE_0[0,4]} & {OE_0[0,5]} & {T_0_sol} \\\\ \n \\hline ")
     plt.show()
 
 if __name__ == "__main__":
