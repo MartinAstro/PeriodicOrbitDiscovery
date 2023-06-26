a4ccf7c265ce0b94c68340aefd672175a07bec13
John M <john.r.martin@colorado.edu>
2023-06-26 11:05:45 -0600

    Update histogram figure

--- Scripts_Orbits/Figures/figure_solution_histogram.py
+++ Scripts_Orbits/Figures/figure_solution_histogram.py
@@ -1,105 +1,84 @@
 import os
 
-import GravNN
 import matplotlib.pyplot as plt
 import numpy as np
-from GravNN.CelestialBodies.Asteroids import Eros
+import pandas as pd
 
 import FrozenOrbits
-from FrozenOrbits.boundary_conditions import *
 from FrozenOrbits.bvp import *
 from FrozenOrbits.constraints import *
-from FrozenOrbits.gravity_models import pinnGravityModel
 from FrozenOrbits.LPE import *
-from FrozenOrbits.utils import propagate_orbit
 from FrozenOrbits.visualization import *
 from Scripts_Orbits.BVP.initial_conditions import *
 
 
-def get_original_IC(df, k):
-    T_0 = df.iloc[k]["T_0"]
-    X_0 = np.array(df.iloc[k]["X_0"])
-    return T_0, X_0
-
-
-def get_corrected_IC(df, k):
-    T_0 = df.iloc[k]["T_0_sol"]
-    X_0 = np.array(df.iloc[k]["X_0_sol"])
-    return T_0, X_0
-
-
-def get_integrated_orbit(T_0, X_0, model, orbit_multiplier=10):
-    t_eval = np.linspace(0, T_0 * orbit_multiplier, int(100 * orbit_multiplier))
-    orbit_sol = propagate_orbit(
-        T_0 * orbit_multiplier, X_0, model, tol=1e-7, t_eval=t_eval
-    )
-    return orbit_sol
-
-
-def gather_integrated_orbits(df):
-    Eros()
-    model = pinnGravityModel(
-        os.path.dirname(GravNN.__file__) + "/../Data/Dataframes/eros_BVP_PINN_III.data"
-    )
-
-    # Generate the raw data for plotting
-    cart_orbit_list = []
-    for k in range(len(df)):
-        T_0_sol, X_0_sol = get_corrected_IC(df, k)
-        corrected_orbit_sol = get_integrated_orbit(T_0_sol, X_0_sol, model)
-        cart_orbit_list.append(corrected_orbit_sol)
-    return cart_orbit_list
-
-
-def plot_solution_histogram(cart_orbit_list, **kwargs):
-    percent_error_list = []
-    position_error_list = []
-    for orbit in cart_orbit_list:
-        X_0 = orbit.y[:, 0]
-        X_f = orbit.y[:, -1]
-        dX = X_f - X_0
-        dX_percent = np.abs(dX / X_0) * 100
-        dX_percent_mag = np.average(dX_percent)
-        percent_error_list.append(dX_percent_mag)
-        position_error_list.append(np.linalg.norm(dX[0:3]))
-
-    # range = kwargs.get('range', [0, np.max(percent_error_list)])
-    range = kwargs.get("range", [0, np.max(position_error_list)])
+def plot_solution_histogram(error, **kwargs):
+    range = kwargs.get("range", [0, np.max(error)])
     kwargs.update({"range": range})
     # range=None
-    print(position_error_list)
     plt.hist(
-        position_error_list,
+        error,
         bins=np.logspace(np.log10(1), np.log10(10**6), 50),
-        #  range=[np.log10(1E-2), np.log10(5E2)],
         **kwargs,
     )
     return range
 
 
 def main():
-    directory = os.path.dirname(FrozenOrbits.__file__) + "/Data/"
-    fine_df = pd.read_pickle(directory + "fine_orbit_solutions.data")
-    cart_fine_df = pd.read_pickle(directory + "cartesian_fine_orbit_solutions.data")
+    directory = os.path.dirname(FrozenOrbits.__file__)
+    df = pd.read_pickle(directory + "/Data/orbit_solutions.data")
 
-    sol_OE = gather_integrated_orbits(fine_df)
-    sol_cart = gather_integrated_orbits(cart_fine_df)
+    # compute percent error between initial and final state
+    def compute_percent_error(key):
+        X_0 = df[key].y[:, 0]
+        X_f = df[key].y[:, -1]
+        dX = X_f - X_0
+        dX_percent = np.abs(dX / X_0) * 100
+        dX_percent_mag = np.average(dX_percent)
+        return dX_percent_mag
+
+    # Save into the dataframe
+    df["OE_percent"] = df.apply(lambda: compute_percent_error("OE_sol"), axis=1)
+    df["X_percent"] = df.apply(lambda: compute_percent_error("X_sol"), axis=1)
+
+    # filter the data based on the solver_key
+    cart_df = df[df["solver_key"] == "cart"]
+    mil_df = df[df["solver_key"] == "mil"]
+    equi_df = df[df["solver_key"] == "equi"]
+    trad_df = df[df["solver_key"] == "trad"]
 
     vis = VisualizationBase()
     vis.newFig()
 
     plot_solution_histogram(
-        sol_cart, color="orange", label="Cartesian Shooting Method", alpha=0.8
+        cart_df["X_percent"],
+        color="orange",
+        label="Cartesian Shooting Method",
+        alpha=0.8,
     )
     plot_solution_histogram(
-        sol_OE, color="blue", label="OE Shooting Method", alpha=0.8
-    )  # , range=range)
+        trad_df["OE_percent"],
+        color="blue",
+        label="Traditional OE Shooting Method",
+        alpha=0.8,
+    )
+    plot_solution_histogram(
+        mil_df["OE_percent"],
+        color="red",
+        label="Milankovitch OE Shooting Method",
+        alpha=0.8,
+    )
+    plot_solution_histogram(
+        equi_df["OE_percent"],
+        color="red",
+        label="Equinoctial OE Shooting Method",
+        alpha=0.8,
+    )
     plt.xscale("log")
     plt.legend()
     plt.xlabel("Cartesian State Error after 10 Orbits [m]")
     plt.ylabel("\# of Solutions")
-    base_figure_name = os.path.basename(__file__).split(".py")[0]
-    vis.save(plt.gcf(), f"{base_figure_name}.pdf")
+    vis.save(plt.gcf(), directory + "/Plots/error_histogram.pdf")
     plt.show()
 
 
